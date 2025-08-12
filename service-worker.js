25f4d118a41d5ddd66000da1365d0dd42d70d6fa
stijndirven <stijndirven@hotmail.nl>
2025-08-12 20:14:56 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Simple service worker for basic offline caching (optional)
 const CACHE = 'hm-coach-v1';
-const FILES = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
+const FILES = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
